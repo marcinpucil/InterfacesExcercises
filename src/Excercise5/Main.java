b98d6aa08e7fe745c3922a1c03dded9c2402ebda
marcinpucil <marcin.pucilowskiii@gmail.com>
2017-09-07 22:35:06 +0200

    InterfacesExcercise5 solved v1.0

--- src/Excercise5/Main.java
+++ src/Excercise5/Main.java
@@ -15,6 +15,19 @@ import java.util.Comparator;
 
 public class Main {
 
+
+    void luckySort(ArrayList<String> strings, Comparator<String> comp) {
+        ArrayList<String> strings1 = new ArrayList<>(strings);
+        strings1.sort(comp);
+        int i = 0;
+        while (Arrays.deepEquals(strings.toArray(), strings1.toArray()) == false) {
+            Collections.shuffle(strings);
+            i++;
+        }
+        System.out.println("Wykonano ileracji:" + i);
+    }
+
+
     public static void main(String[] args) {
         Main main = new Main();
         ArrayList<String> strings = new ArrayList<>();
@@ -25,30 +38,5 @@ public class Main {
         strings.add("dziala");
         main.luckySort(strings, Comparator.naturalOrder());
 
-        main.test();
-    }
-
-    void test() {
-        ArrayList<String> strings1 = new ArrayList<>();
-        ArrayList<String> strings2 = new ArrayList<>(strings1);
-        strings1.add("c");
-        strings1.add("s");
-        strings1.add("d");
-        System.out.println(strings1);
-        //strings2.sort(Comparator.naturalOrder());
-        System.out.println(strings2);
-        System.out.println(strings1 == strings2);
-        System.out.println(Arrays.equals(strings1.toArray(), strings2.toArray()));
-    }
-
-    void luckySort(ArrayList<String> strings, Comparator<String> comp) {
-        ArrayList<String> strings1 = new ArrayList<>(strings);
-        strings1.sort(comp);
-        int i = 0;
-        while (Arrays.deepEquals(strings.toArray(), strings1.toArray())) {
-            Collections.shuffle(strings);
-            i++;
-        }
-        System.out.println(i);
     }
 }
