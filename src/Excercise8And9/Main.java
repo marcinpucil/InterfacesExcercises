b81510388977c41cffde369841bccc7d66eef5f4
marcinpucil <marcin.pucilowskiii@gmail.com>
2017-09-10 23:45:48 +0200

    InterfacesExcercise9 notsolved v.0.1

--- src/Excercise8And9/Main.java
+++ src/Excercise8And9/Main.java
@@ -1,4 +1,3 @@
-
 /*Excercise8
 Korzystając z metod listFiles(FileFilter) i isDirectory z klasy java.io.File,
 napisz metodę zwracającą wszystkie podkatalogi wskazanego katalogu. Wykorzystaj
@@ -9,19 +8,42 @@ Korzystając z metody list(FilenameFilter) klasy java.io.File, napisz metodę
 zwracającą wszystkie pliki ze wskazanego katalogu ze wskazanym rozszerzeniem.
 Użyj wyrażenia lambda, a nie FilenameFilter. Jakie zmienne zewnętrzne
 wykorzystasz.
-Excercise10
-Mając tablicę obiektów File, posortuj je w taki sposób, by katalogi znalazły się
-przed plikami, a w każdej grupie elementy zostały posortowane według nazwy.
-Użyj wyrażenia lambda przy implementowaniu interfejsu Comparator.
  */
+
 
 package Excercise8And9;
 
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.stream.Stream;
+
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        Stream.of(main.getSubdirectories1("D:\\nauka programowania")).forEach(file -> System.out.println(file.getAbsoluteFile()));
+        Stream.of(main.getSubdirectories2("D:\\nauka programowania")).forEach(file -> System.out.println(file.getAbsoluteFile()));
+    }
+
+    File[] getSubdirectories2(String dirAddress) {
+        return new File(dirAddress).listFiles(File::isDirectory);
+    }
+
+    File[] getSubdirectories1(String dirAddress) {
+        return new File(dirAddress).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+        });
+    }
+
+    public File[] getFilesByExtension(String direction, String extension) {
+        //TODO
+        return null;
     }
 
 }
